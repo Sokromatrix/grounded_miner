ab8606e51d44b12e70f63d9cdea65d5a9269257a
Sokromatrix <kroschewski1971@gmail.com>
2026-08-25 17:35:43 +0200

    Initial commit

---
+++ src/extract.rs
@@ -0,0 +1,47 @@
+use std::fs;
+const CREATURES_DIR: &str = "/Maine/Content/Blueprints/Creatures";
+
+pub fn extract_creature_damage_modifiers(path_to_maine: &str) {
+    println!(
+        "Extracting creature damage modifiers from {}",
+        format!("{}{}", path_to_maine, CREATURES_DIR)
+    );
+
+    let mut files: Vec<String> = Vec::new();
+    resolve_path(path_to_maine, "", &mut files);
+    for entry in files {
+        if fs::read_to_string(&entry).unwrap().contains("DamageResist") {
+            println!("{:?}", entry);
+        }
+    }
+}
+
+fn resolve_path(path_to_maine: &str, relative_path: &str, files: &mut Vec<String>) {
+    let path = format!("{}{}{}", path_to_maine, CREATURES_DIR, relative_path);
+    match fs::metadata(&path) {
+        Ok(metadata) => {
+            if metadata.is_dir() {
+                let entries = fs::read_dir(&path).unwrap();
+                for entry in entries {
+                    match entry {
+                        Ok(entry) => {
+                            resolve_path(
+                                path_to_maine,
+                                &format!(
+                                    "{}/{}",
+                                    relative_path,
+                                    entry.file_name().to_str().unwrap()
+                                ),
+                                files,
+                            );
+                        }
+                        Err(_) => {}
+                    }
+                }
+            } else {
+                files.push(String::from(relative_path));
+            }
+        }
+        Err(_) => {}
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+const PATH_TO_MAINE: &str = "/Applications/Output/Exports";
+
+mod extract;
+
+fn main() {
+    println!("Hello, world!");
+    extract::extract_creature_damage_modifiers(&format!(
+        "{}{}",
+        directories::UserDirs::new()
+            .unwrap()
+            .home_dir()
+            .to_str()
+            .unwrap(),
+        PATH_TO_MAINE,
+    ));
+}
